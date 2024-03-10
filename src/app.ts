c7e7eef0929496ccf2f8a24e6e00271d6a34feca
nayanrdeveloper <nayanrdeveloper@gmail.com>
2024-03-10 11:05:28 +0530

    add error-middleware and mogodb config

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,21 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
+import express, { Application } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import paletteRoute from '../src/routes/paletteroute';
-import mongoose from 'mongoose';
+import { errorHandler } from './middlewares/errorMiddlewares';
+import connectToDatabase from './config/databaseConfig';
 
 const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use(errorHandler);
 app.use('/api', paletteRoute);
 
-mongoose
-    .connect(process.env.MONGODB_URI || '', {})
-    .then(() => {
-        console.log('Connected to MongoDB');
-        app.listen(3000, () => {
-            console.log(`Server is running on port ${3000}`);
-        });
-    })
-    .catch(error => console.error(error));
+const PORT: number = parseInt(process.env.PORT || '4000', 10);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+connectToDatabase();
